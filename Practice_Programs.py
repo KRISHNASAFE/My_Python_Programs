ec1b8630fc598aa47d4a21f6087b7f1bf6e4ce10
KRISHNASAFE <163742545+KRISHNASAFE@users.noreply.github.com>
2024-12-01 14:26:10 +0530

    Create Practice_Programs.py

---
+++ Practice_Programs.py
@@ -0,0 +1,15 @@
+"""
+# 1. Word Replacement Program :
+
+def replace_word():
+
+   str = "Hi Guys, How are You? ...... HI HI HI HI HI HI HI HI !!! "
+   print(str)
+   print("Enter the word to replace from above string.")
+   word_to_replace = input("Enter word to replace:")
+   word_replacement = input("Enter the word replacement:")
+   print(str.replace(word_to_replace, word_replacement))
+
+
+replace_word()
+"""
